5e6effee920c6487c9ead7013694c6d51725a7a6
rishabh jain <goyal.rishabh12@gmail.com>
2023-02-23 21:55:21 +0530

    fir

--- rout1/src/Jeans.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Jeans() {
-  return (
-    <div>
-      <h1>all jeans items defined</h1>
-    </div>
-  )
-}
--- rout1/src/PagenotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PagenotFound() {
-  return (
-    <div>
-      <h1> 404 poge not found </h1>
-    </div>
-  )
-}
